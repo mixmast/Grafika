92d51b118cc628a549dbe3ed33ff299ad7d6d5e3
przemek19873212@gmail.com <przemek19873212@gmail.com>
2021-06-30 21:59:52 +0200

    Ulepszenie shape

--- Shape.h
+++ Shape.h
@@ -180,43 +180,33 @@ inline std::string Shape::txt_code() {
 	switch (_kind_of_shape) {
 
 	case CIRCLE:
-		r = sqrt(pow(_points[1].x - _points[0].x, 2) + pow(_points[1].y - _points[0].y, 2));
-		shape_code += std::to_string(static_cast<int>(_points[0].x - r)) + " " + std::to_string(static_cast<int>(_points[0].y - r));
+		shape_code += std::to_string(static_cast<int>(_points[0].x )) + " " + std::to_string(static_cast<int>(_points[0].y ));
 		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x - r)) + " " + std::to_string(static_cast<int>(_points[0].y + r));
+		shape_code += std::to_string(static_cast<int>(_points[1].x )) + " " + std::to_string(static_cast<int>(_points[1].y));
 		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x + r)) + " " + std::to_string(static_cast<int>(_points[0].y + r));
-		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x + r)) + " " + std::to_string(static_cast<int>(_points[0].y - r));
 		break;
 
 	case SQUARE:
-		a = std::max(_points[1].x - _points[0].x, _points[1].y - _points[0].y);
 		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y));
-		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y + a));
 		shape_code += " ";
 		shape_code += std::to_string(static_cast<int>(_points[1].x)) + " " + std::to_string(static_cast<int>(_points[1].y));
 		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y - a));
 		break;
 
 	case ELLIPSE:
-		a = _points[1].y - _points[0].y;
 		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y));
-		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y + a));
 		shape_code += " ";
 		shape_code += std::to_string(static_cast<int>(_points[1].x)) + " " + std::to_string(static_cast<int>(_points[1].y));
 		shape_code += " ";
-		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y - a));
 		break;
 
 	case TRIANGLE:
-		for (int i = 0; i < 3; ++i) {
-			shape_code += std::to_string(static_cast<int>(_points[i].x)) + " " + std::to_string(static_cast<int>(_points[i].y));
-			shape_code += " ";
-		}
+		
+		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y));
+		shape_code += " ";
+		shape_code += std::to_string(static_cast<int>(_points[1].x)) + " " + std::to_string(static_cast<int>(_points[1].y));
+		shape_code += " ";
+		
 		break;
 
 	case CURVE_LINE:
@@ -227,10 +217,10 @@ inline std::string Shape::txt_code() {
 		break;
 
 	case BROKEN_LINE:
-		for (auto point : _points) {
-			shape_code += std::to_string(static_cast<int>(point.x)) + " " + std::to_string(static_cast<int>(point.y));
-			shape_code += " ";
-		}
+		shape_code += std::to_string(static_cast<int>(_points[0].x)) + " " + std::to_string(static_cast<int>(_points[0].y));
+		shape_code += " ";
+		shape_code += std::to_string(static_cast<int>(_points[1].x)) + " " + std::to_string(static_cast<int>(_points[1].y));
+		shape_code += " ";
 		break;
 
 	default:
